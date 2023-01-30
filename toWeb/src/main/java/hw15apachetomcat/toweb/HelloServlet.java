0fa7334d920cda9b81a6a4c3e9550200c35a265a
mariiasuraieva <suraieva.m@icloud.com>
2023-01-30 15:47:45 +0100

    Done

---
+++ toWeb/src/main/java/hw15apachetomcat/toweb/HelloServlet.java
@@ -0,0 +1,14 @@
+package hw15apachetomcat.toweb;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello World!");
+    }
+}
